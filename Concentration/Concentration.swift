ae09d31b1b626a6f565eaf37710c2383f4930ae3
bijan102 <bfazeli1@gmail.com>
2018-03-21 21:18:34 -0700

    Update: Refactored choose cards to account for matched cards.

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -11,12 +11,26 @@ import Foundation
 // Classes are reference type
 class Concentration {
     var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
     
     func chooseCard(at index: Int) {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
+        if !cards[index].isMatched {
+            if let matchedIndex = indexOfOneAndOnlyFaceUpCard, matchedIndex != index {
+                // Check if cards match
+                if cards[matchedIndex].identifier == cards[index].identifier {
+                    cards[matchedIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // Either no cards or two cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     
